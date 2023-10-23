4455c6b07c8cf25218a6754e1a1d66b3957f7480
Jordan Anderson <tattooguy68w@gmail.com>
2023-10-23 13:11:56 -0400

    first d3 project

---
+++ script.js
@@ -0,0 +1,67 @@
+//chart data
+const data = [
+    { country: "United States", population: 332 },
+    { country: "China", population: 1444 },
+    { country: "India", population: 1392 },
+    { country: "Indonesia", population: 276 },
+    { country: "Pakistan", population: 225 },
+    { country: "Brazil", population: 213 },
+    { country: "Nigeria", population: 211 },
+    { country: "Bangladesh", population: 166 },
+    { country: "Russia", population: 146 },
+    { country: "Mexico", population: 130 }
+];
+
+//chart dimensions and scales
+const margin = { top: 20, right: 20, bottom: 60, left: 60 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+//band scale for x-axis
+const x = d3.scaleBand()
+    .domain(data.map(d => d.country))
+    .range([0, width])
+    .padding(0.2);
+//linear scale for y-axis
+const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([height, 0]);
+
+//create svg element
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom +20)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+//rectangles for each data point
+svg.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.country))
+    .attr("width", x.bandwidth())
+    .attr("y", d => y(d.population))
+    .attr("height", d => height - y(d.population))
+    .attr("fill", "steelblue")
+    .on("mouseover", function (d) {
+        d3.select(this).style("fill", "#5db0f5")
+    })
+    .on("mouseout", function (d) {
+        d3.select(this).style("fill", "steelblue")
+    });
+
+//add x-axis labels
+svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-65)");
+//add y-axis label
+svg.append("g")
+    .call(d3.axisLeft(y));
